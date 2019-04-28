e07c1c867004ee0f8c32afeceac086aa1b451783
Victor Quiroz Castro <victorhqc@gmail.com>
2019-04-28 12:09:09 +0200

    Hide private fuunctions

--- src/lib.rs
+++ src/lib.rs
@@ -44,13 +44,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents.lines()
         .filter(|line| line.contains(query))
         .collect()
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
 
     contents.lines()
